2343c8449a82313d4ad77c267f51bab3a8687976
Ibramsou <kasobanboui@gmail.com>
2023-01-15 18:17:31 +0100

    Setup gradle + plugin base

---
+++ src/main/java/fr/bramsou/messaging/bukkit/BukkitMessaging.java
@@ -0,0 +1,6 @@
+package fr.bramsou.messaging.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitMessaging extends JavaPlugin {
+}
---
+++ src/main/java/fr/bramsou/messaging/proxy/ProxyMessaging.java
@@ -0,0 +1,6 @@
+package fr.bramsou.messaging.proxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ProxyMessaging extends Plugin {
+}
